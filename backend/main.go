0abf87a015e40d69f296b05490716b3100fcbab0
Gonçalo Castro <goncalo122016@gmail.com>
2026-02-14 12:11:15 +0000

    [feat]: Implementing deploy with Render

--- backend/main.go
+++ backend/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"rummikub/api"
 )
 
 func main() {
-	fmt.Println("Starting Rummikub Game Server...")
 
-	http.HandleFunc("/ws", api.WSHandler)
-
-	// Render define a porta via variável de ambiente
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080" // fallback para desenvolvimento local
+		port = "8080"
 	}
 
-	fmt.Println("Server running on port :", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// WebSocket
+	http.HandleFunc("/ws", api.WSHandler)
+
+	// Servir frontend
+	fs := http.FileServer(http.Dir("./dist"))
+	http.Handle("/", fs)
+
+	http.ListenAndServe(":"+port, nil)
 }
