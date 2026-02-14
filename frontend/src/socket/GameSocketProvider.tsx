9199a26e73f9467d1e93b78e6edde6084388af68
Gonçalo Castro <goncalo122016@gmail.com>
2026-02-14 12:11:15 +0000

    [feat]: Implementing deploy with Render

--- frontend/src/socket/GameSocketProvider.tsx
+++ frontend/src/socket/GameSocketProvider.tsx
@@ -12,8 +12,10 @@ export function GameSocketProvider({
   const [ready, setReady] = useState(false);
 
   useEffect(() => {
-    const wsUrl = import.meta.env.VITE_MODE === "production" ? import.meta.env.VITE_WS_URL : `${window.location.protocol === "https:" ? "wss" : "ws"}://localhost:8080/ws`
-      
+    const protocol = window.location.protocol === "https:" ? "wss" : "ws"
+    const host = window.location.host
+    const wsUrl = `${protocol}://${host}/ws`
+
     console.log("Connecting to WebSocket...", wsUrl)
     const ws = new WebSocket(wsUrl)
 
